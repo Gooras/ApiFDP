7b03029cd750326881d08fd58c0adb6dd2731837
Gooras <gooras76@gmail.com>
2020-05-21 12:43:46 +0200

    Basic Authentication
    Posprzątanie delikatne + Basic Authentication - jakieś początki (sztywniak - użytkownika user:password + Base64)

--- src/main/java/com/asseco/cm/FDPProxyXMLController.java
+++ src/main/java/com/asseco/cm/FDPProxyXMLController.java
@@ -68,11 +68,16 @@ public class FDPProxyXMLController {
     URL url = new URL(clientConf.getConfFdpWsdlUrl());
     //URL url = new URL(conf.getWsAddress());
     //url = conf.getWsdl();
-    log.debug("(Bean) (CardService) Serwis utworzony "+url);
+    log.debug("(Bean) (CardService) WSDL: "+url);
+    CardService_Service service = null;
 
     //TODO: zgłosić wyjątek, gdy nie uda się stworzyć serwisu? Da się to jakoś sprawdzić?
-    CardService_Service service = new CardService_Service(url);
-    log.debug("(Bean) (CardService) Serwis utworzony "+url);
+    try {
+      service = new CardService_Service(url);
+    } catch (Exception e) {
+      log.error("(Bean) (CardService) Błąd tworzenia serwisu "+url+e.getLocalizedMessage());
+    }
+
     if (service==null) {
       log.error("(Bean) (CardService) Błąd tworzenia serwisu "+url);
     } else
@@ -91,99 +96,6 @@ public class FDPProxyXMLController {
     addInterceptors(cli);
     log.debug("(Bean) Klient - interceptory włączone");
     return cli;
-  }
-
-  private void addInterceptors(Client cli) {
-
-    if (clientConf.getConfWSSEnabled()) {
-
-    if (conf.getLoggingInEnabled()) {
-      myLog("DemoController - interceptory do logowania - In");
-      LoggingInInterceptor loggerIn = new LoggingInInterceptor();
-        PrintWriter writerIn = null;
-        try {
-          writerIn = new PrintWriter(new File("src/main/resources/FDPProxyIn.log"));
-          //writerIn = new PrintWriter(new File("C:/Users/Grzegorz.Gora/Desktop/REST/FDPapi/src/main/resources/FDPProxyIn.log"));
-        } catch (FileNotFoundException e) {
-          log.error(e.getLocalizedMessage());
-          e.printStackTrace();
-        }
-
-        loggerIn.setPrintWriter(writerIn);
-        cli.getInInterceptors().add(loggerIn);
-      }
-
-    if (conf.getLoggingOutEnabled()) {
-      myLog("DemoController - interceptory do logowania - Out");
-        PrintWriter writerOut = null;
-        Date date = Calendar.getInstance().getTime();
-        DateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd_hh-mm-ss");
-        String strDate = dateFormat.format(date);
-        String fileName = conf.getLoggingOutFileName()+"_"+strDate+".log";
-        try {
-          File file = new File("/logs/"+fileName);
-          log.debug("Path1: "+file.getAbsolutePath());
-          log.debug("Writer: "+file.getCanonicalPath());
-          writerOut = new PrintWriter(file);
-          file = new File(fileName);
-          log.debug("Path2: "+file.getAbsolutePath());
-          log.debug("Writer: "+file.getCanonicalPath());
-          writerOut = new PrintWriter(file);
-          file = new File("../log/"+fileName);
-          log.debug("Path3: "+file.getAbsolutePath());
-          log.debug("Writer: "+file.getCanonicalPath());
-          file = new File("./log/"+fileName);
-          log.debug("Path4: "+file.getAbsolutePath());
-          log.debug("Writer: "+file.getCanonicalPath());
-          writerOut = new PrintWriter(file);
-
-//          writerOut = new PrintWriter(new File("src/main/resources/FDPProxyOut.log"));
-        } catch (FileNotFoundException e) {
-          log.error(e.getLocalizedMessage());
-          e.printStackTrace();
-        } catch (IOException e) {
-          log.error(e.getLocalizedMessage());
-          e.printStackTrace();
-        }
-
-        LoggingOutInterceptor loggerOut = new LoggingOutInterceptor(writerOut);
-        cli.getOutInterceptors().add(loggerOut);
-      }
-
-    }//koniec warunku 1=0
-
-  }
-
-  // wyłączony Konstruktor - inicjalizacja serwisu FDP
-  public void testFDPProxyXMLController() {
-/*    try {
-      System.out.println("FDPProxyXMLController - konstruktor");
-//      wsdl_url = new URL("CardService.wsdl");
-//      wsdl_url = new File("src/main/resources/CardService.wsdl").toURI().toURL();
-//      wsdl_url = new File("/WEB-INF/classes/CardService.wsdl").toURI().toURL();
-//      wsdl_url = new File("C:/app/wildfly-14.0.1.Final/bin/CardService.wsdl").toURI().toURL();
-      wsdl_url = new File("C:/Users/Grzegorz.Gora/Desktop/REST/FDPapi/src/main/resources/CardService.wsdl").toURI().toURL();
-//          wsdl_url = new File(wsdlUrl).toURI().toURL();
-
-      System.out.println("URL: " + wsdl_url);
-    } catch (MalformedURLException e) {
-      java.util.logging.Logger.getLogger(CardService_Service.class.getName())
-          .log(java.util.logging.Level.INFO,
-              "Can not initialize the default wsdl from {0}",
-              wsdl_url);
-
-    }
-*/
-    wsdl_url = ClassLoader.getSystemResource("CardService.wsdl");
-    System.out.println("wsdl: "+wsdl_url);
-    //wsdl_url = getWsld();
-    cService = new CardService_Service(wsdl_url,
-        new QName("http://www.firstdata.pl/wdx/business/card/v5/", "CardService"));
-
-    cardService = cService.getCardServicePort();
-
-    System.out.println("FDPProxyXMLController - konstruktor wykonany, serwis utworzony" + cardService.toString());
-
   }
 
 //Właściwe metody REST
@@ -236,6 +148,67 @@ public class FDPProxyXMLController {
     return "ver: "+conf.getAppVersion()
         + nl + "<br/>logger: "+ log.getClass().getName()
         + nl + "<br/>conf: "+clientConf.getConfFilename();
+  }
+
+  private void addInterceptors(Client cli) {
+
+    if (clientConf.getConfWSSEnabled()) {
+
+      if (conf.getLoggingInEnabled()) {
+        myLog("DemoController - interceptory do logowania - In");
+        LoggingInInterceptor loggerIn = new LoggingInInterceptor();
+        PrintWriter writerIn = null;
+        try {
+          writerIn = new PrintWriter(new File("src/main/resources/FDPProxyIn.log"));
+          //writerIn = new PrintWriter(new File("C:/Users/Grzegorz.Gora/Desktop/REST/FDPapi/src/main/resources/FDPProxyIn.log"));
+        } catch (FileNotFoundException e) {
+          log.error(e.getLocalizedMessage());
+          e.printStackTrace();
+        }
+
+        loggerIn.setPrintWriter(writerIn);
+        cli.getInInterceptors().add(loggerIn);
+      }
+
+      if (conf.getLoggingOutEnabled()) {
+        myLog("DemoController - interceptory do logowania - Out");
+        PrintWriter writerOut = null;
+        Date date = Calendar.getInstance().getTime();
+        DateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd_hh-mm-ss");
+        String strDate = dateFormat.format(date);
+        String fileName = conf.getLoggingOutFileName()+"_"+strDate+".log";
+        try {
+          File file = new File("/logs/"+fileName);
+          log.debug("Path1: "+file.getAbsolutePath());
+          log.debug("Writer: "+file.getCanonicalPath());
+          writerOut = new PrintWriter(file);
+          file = new File(fileName);
+          log.debug("Path2: "+file.getAbsolutePath());
+          log.debug("Writer: "+file.getCanonicalPath());
+          writerOut = new PrintWriter(file);
+          file = new File("../log/"+fileName);
+          log.debug("Path3: "+file.getAbsolutePath());
+          log.debug("Writer: "+file.getCanonicalPath());
+          file = new File("./log/"+fileName);
+          log.debug("Path4: "+file.getAbsolutePath());
+          log.debug("Writer: "+file.getCanonicalPath());
+          writerOut = new PrintWriter(file);
+
+//          writerOut = new PrintWriter(new File("src/main/resources/FDPProxyOut.log"));
+        } catch (FileNotFoundException e) {
+          log.error(e.getLocalizedMessage());
+          e.printStackTrace();
+        } catch (IOException e) {
+          log.error(e.getLocalizedMessage());
+          e.printStackTrace();
+        }
+
+        LoggingOutInterceptor loggerOut = new LoggingOutInterceptor(writerOut);
+        cli.getOutInterceptors().add(loggerOut);
+      }
+
+    }//koniec warunku 1=0
+
   }
 
 }
